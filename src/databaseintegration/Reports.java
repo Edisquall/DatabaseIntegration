cef5536a856a29b6e1bcec515633395d61f7b438
Edisquall <152781383+Edisquall@users.noreply.github.com>
2024-03-24 11:36:42 +0000

    Report
    Created the CSV Report

--- src/databaseintegration/Reports.java
+++ src/databaseintegration/Reports.java
@@ -4,10 +4,41 @@
  */
 package databaseintegration;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author Edi
  */
 public class Reports {
-    
+
+    public void generateTxtReport(List<String> data) {
+        //generating TXT file
+        try ( FileWriter writer = new FileWriter("report.txt")) {
+            for (String line : data) {
+                writer.write(line + "\n");
+            }
+            System.out.println("Txt report generated successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void generateCsvReport(List<String[]> data) {
+        //generating CSV file
+        try ( FileWriter writer = new FileWriter("report.csv")) {
+            for (String[] row : data) {
+                StringBuilder line = new StringBuilder();
+                for (String field : row) {
+                    line.append(field).append(",");
+                }
+                writer.write(line.toString() + "\n");
+            }
+            System.out.println("CSV report generated successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
