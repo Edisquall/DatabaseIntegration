9d95ac18944335d1f0e07fadb036118e6ecb22d3
Edisquall <152781383+Edisquall@users.noreply.github.com>
2024-03-24 10:45:16 +0000

    Connector
    Creating the DB connector through Java

---
+++ src/databaseintegration/DBConnector.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package databaseintegration;
+
+/**
+ *
+ * @author Edi
+ */
+public class DBConnector {
+    
+    private final String DB_URL = "jdbc:mysql://localhost";
+    private final String USER = "pooa2024";
+    private final String PASSWORD = "pooa2024";
+}
